f3afbf4099d81a598d83687c339ea05d621b2981
Tharoon Balaji <tharoonbalaji@gmail.com>
2022-03-18 16:24:54 -0400

    svg

--- src/components/DisplayNFT.js
+++ src/components/DisplayNFT.js
@@ -10,18 +10,16 @@ let contract;
 const traitAddress = '0x8Fad2dD108e3dC171E8271CDF8B9EA768260Ea5f';
 const godAddress = '0x66A7De7572B70850BB6cC0a5DFf03dD3E93E1da6';
 const { ethereum } = window;
-const [svgDrawn, setSVG] = useState();
+const [svgDrawn, setSVG] = useState("");
 
 async function getMinted() {
     const provider = new ethers.providers.Web3Provider(ethereum);
     contract = new ethers.Contract(godAddress, god.abi, provider);
     return await contract.minted();
-    //return amount;
 
 }
 
 async function getSVG() {
-
     const provider = new ethers.providers.Web3Provider(ethereum);
     contract = new ethers.Contract(traitAddress, traits.abi, provider);
     return await contract.drawSVG(3);
