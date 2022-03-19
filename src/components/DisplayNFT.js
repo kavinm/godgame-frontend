d3161b2631c8146086b371335b00ee0654b27ae0
kavinm <30965883+kavinm@users.noreply.github.com>
2022-03-18 21:00:24 -0400

    fixed display NFT

--- src/components/DisplayNFT.js
+++ src/components/DisplayNFT.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./displayNFT.css";
 import traits from "./utils/Traits.json";
 import Slider from "react-input-slider";
@@ -9,37 +9,22 @@ let contract;
 const traitAddress = "0x8Fad2dD108e3dC171E8271CDF8B9EA768260Ea5f";
 const godAddress = "0x66A7De7572B70850BB6cC0a5DFf03dD3E93E1da6";
 const { ethereum } = window;
+let currentAccount;
+var svgDrawn;
 
-async function getMinted() {
-  const provider = new ethers.providers.Web3Provider(ethereum);
-  contract = new ethers.Contract(godAddress, god.abi, provider);
-  return contract.minted();
-  //return amount;
-}
+// async function getMinted() {
+//   const provider = new ethers.providers.Web3Provider(ethereum);
+//   contract = new ethers.Contract(godAddress, god.abi, provider);
+//   return contract.minted();
+//   //return amount;
+// }
 
-async function getSVG(num) {
-  const provider = new ethers.providers.Web3Provider(ethereum);
-  contract = new ethers.Contract(traitAddress, traits.abi, provider);
-  return contract.drawSVG(num);
-}
+// async function getSVG(token) {
+//   const provider = new ethers.providers.Web3Provider(ethereum);
+//   contract = new ethers.Contract(traitAddress, traits.abi, provider);
+//   svgDrawn = await contract.drawSVG(token);
+// }
 const DisplayNFT = () => {
-  const [svgDrawn, setSVG] = useState({ svg: "" });
-  useEffect(() => {
-    getSVG().then((res) => {
-      setSVG(getSVG(5));
-    });
-  });
-  console.log("svg: ", getSVG(3));
-  setSVG(5);
-  console.log("draw: ", svgDrawn);
-  //console.log(getMinted());
-  return (
-    <div>
-      CHUNGUS
-      <img src={svgDrawn} />
-    </div>
-  );
-
   const [viewNfts, setViewNfts] = useState(false);
 
   let [ownedSvgs, setOwnedSvgs] = useState([{ svg: "", token_id: 0 }]);
@@ -113,9 +98,5 @@ const DisplayNFT = () => {
       })}
     </div>
   );
-
-  //     {viewNfts && (<img src={ownedSvgs[8].svg}></img>)}
-  //   </div>
-  // );
 };
 export default DisplayNFT;
