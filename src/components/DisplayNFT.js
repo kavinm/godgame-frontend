a7873796a7f0a50f042d87f0639287b4a8319e19
kavinm <30965883+kavinm@users.noreply.github.com>
2022-03-18 20:07:29 -0400

    initial display svg function

--- src/components/DisplayNFT.js
+++ src/components/DisplayNFT.js
@@ -9,6 +9,7 @@ let contract;
 const traitAddress = "0x8Fad2dD108e3dC171E8271CDF8B9EA768260Ea5f";
 const godAddress = "0x66A7De7572B70850BB6cC0a5DFf03dD3E93E1da6";
 const { ethereum } = window;
+let currentAccount;
 var svgDrawn;
 
 // async function getMinted() {
@@ -24,7 +25,74 @@ var svgDrawn;
 //   svgDrawn = await contract.drawSVG(token);
 // }
 const DisplayNFT = () => {
-  //console.log(getMinted());
-  return <div>CHUNGUS</div>;
+  const [viewNfts, setViewNfts] = useState(false);
+
+  let [ownedSvgs, setOwnedSvgs] = useState([{ svg: "", token_id: 0 }]);
+
+  const returnTokenSvgs = async () => {
+    const tokenIds = [];
+    const tokenSvgs = [];
+
+    if (ethereum) {
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const connectedContract = new ethers.Contract(
+        godAddress,
+        god.abi,
+        provider
+      );
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      currentAccount = accounts[0];
+
+      const ownedTokenAmount = await connectedContract.balanceOf(
+        currentAccount
+      );
+      console.log(ownedTokenAmount);
+
+      for (let i = 0; i < ownedTokenAmount; i += 1) {
+        const tokenId = await connectedContract.tokenOfOwnerByIndex(
+          currentAccount,
+          i
+        );
+        tokenIds.push(tokenId);
+        console.log(tokenId);
+      }
+
+      for (const tokenId of tokenIds) {
+        const rawUri = await connectedContract.tokenURI(tokenId);
+
+        const decodedUri = rawUri.split(",")[1];
+
+        const encodedSvg = JSON.parse(atob(decodedUri))["image"];
+        tokenSvgs.push({
+          token_id: tokenId,
+          svg: encodedSvg,
+        });
+      }
+    }
+
+    console.log(
+      "This is the id: " +
+        tokenSvgs[0].token_id +
+        "and svg is: " +
+        tokenSvgs[0].svg
+    );
+    return tokenSvgs;
+  };
+
+  // return (
+  //   <div>
+  //     <button
+  //       onClick={async () => {
+  //         setViewNfts(true);
+  //         const heldTokens = await returnTokenSvgs(currentAccount);
+  //         setOwnedSvgs(heldTokens);
+  //       }}></button>
+
+  //     {viewNfts && (<img src={ownedSvgs[8].svg}></img>)}
+  //   </div>
+  // );
 };
 export default DisplayNFT;
